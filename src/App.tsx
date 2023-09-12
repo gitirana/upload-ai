612077fea9fe6d9ce24c1c1602a6b72d5c418dfe
gitirana <thaygitirana@gmail.com>
2023-09-11 21:04:42 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 export function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <Button variant={"outline"}>
+          <Github className="w-4 h-4 mr-2" />
+          Github
+        </Button>
+      </header>
+    </div>
   )
 }
 
