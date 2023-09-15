46edf3aaae8cfc7a68b06eee83f0b2075061a11e
gitirana <thaygitirana@gmail.com>
2023-09-15 00:00:10 -0300

    feat: create video input component and preview, and start ffmpeg config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
+import { Github, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Textarea } from "./components/ui/textarea";
 import { Separator } from "./components/ui/separator";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
 
 export function App() {
 
@@ -30,29 +31,7 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-
-            <input type="file" id="video" accept="video/mp4" className="sr-only" />
-
-            <Separator />
-
-            <section className="space-y-2">
-              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-              <Textarea id="transcription_prompt" className="resize-y h-20 leading-relaxed" placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula" />
-            </section>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
@@ -89,8 +68,6 @@ export function App() {
 
               <span className="block text-xs text-muted-foreground italic">Você poderá customizar essa opção em breve</span>
             </section>
-
-            <Separator />
 
             <section className="space-y-5">
               <Label>Temperatura</Label>
